89ad48b0306574f3dc38fd2a71b061e7e2e6b1dd
Pål Hamel <pal.hamel@gmail.com>
2020-11-21 12:40:08 +0100

    finish combineReducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 // static array
 const songsReducer = () => {
   return [
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
