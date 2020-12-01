eaf77eda865512bc86a5ce08c4792e0b471ee84d
Pål Hamel <pal.hamel@gmail.com>
2020-12-01 15:41:48 +0100

    add SongDetail to app

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,4 +1,19 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
+// component magic:
 
+const SongDetail = (props) => {
+  console.log(props);
+  return <div>Song details here</div>;
+};
+
+// data från state to connect:
+const mapStateToProps = (state) => {
+  // console.log(state);
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
+
+// export default connect(mapStateToProps, { selectSong })(SongList);
